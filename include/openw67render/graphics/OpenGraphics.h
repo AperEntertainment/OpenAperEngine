b66a54eda3bdda39357f9f9776d28f978aab4fce
w67clement <w67clement@gmail.com>
2016-10-18 23:08:54 +0200

    Some new things for graphics.

--- include/openw67render/graphics/OpenGraphics.h
+++ include/openw67render/graphics/OpenGraphics.h
@@ -21,20 +21,68 @@
 #define CPPW67RENDER_OPENGRAPHICS_H
 
 #include "Color.h"
+#include "../window/OpenWindow.h"
 
 namespace Openw67Render
 {
     class OpenGraphics
     {
     private:
-        float complementX = 0.0;
-        float complementY = 0.0;
+        float _complementX = 0.0;
+        float _complementY = 0.0;
         bool drawing = false;
 
     public:
         OpenGraphics(float complementX = 0.0, float complementY = 0.0);
 
+        /*! @brief Gets the complement X of the OpenGraphics.
+         *
+         * This function returns the complement X of the OpenGraphics.
+         * Complement X is used for the coordinates of some drawn objects
+         *
+         * @return Complement X of the OpenGraphics.
+         */
+        float complementX() const;
+
+        /*! @brief Gets the complement Y of the OpenGraphics.
+         *
+         * This function returns the complement Y of the OpenGraphics.
+         * Complement Y is used for the coordinates of some drawn objects
+         *
+         * @return Complement Y of the OpenGraphics.
+         */
+        float complementY() const;
+
         void setColor(Color color);
+
+        /*! @brief Enables 2D rendering.
+         *
+         * This function set OpenGL to render in flat 2D (no perspective) on top of current scene.
+         * Preserve current projection and model views, and disable depth testing.
+         * Ortho world size will be same as viewport size, so any ortho  will be scaled to fit viewport.
+         *
+         * @param window Window to get size.
+         */
+        void setOrthoOn(OpenWindow window);
+
+        /*! @brief Disables 2D rendering.
+         *
+         * This function turns 2D mode off.
+         * It return the projection and model views to their preserved state was saved when setOrthoOn() was called,
+         * and re-enable depth testing.
+         *
+         */
+        void setOrthoOff();
+
+        /*! @brief Enables or disables the transparency.
+         *
+         * This function enable or disable the OpenGL transparency.
+         * It enables the transparency when the parameter transparency is true and it disables the transparency
+         * when the paramenter transparency is false.
+         *
+         * @param transparency True if enable the transparency, else false.
+         */
+        void setTransparency(bool transparency);
 
         void beginDraw(unsigned int drawType);
 
--- src/graphics/OpenGraphics.cpp
+++ src/graphics/OpenGraphics.cpp
@@ -23,9 +23,62 @@
 
 namespace Openw67Render
 {
-    OpenGraphics::OpenGraphics(float complementX, float complementY) : complementX(complementX),
-                                                                       complementY(complementY)
+    OpenGraphics::OpenGraphics(float complementX, float complementY) : _complementX(complementX),
+                                                                       _complementY(complementY)
     {
+    }
+
+    float OpenGraphics::complementX() const
+    {
+        return _complementX;
+    }
+
+    float OpenGraphics::complementY() const
+    {
+        return _complementY;
+    }
+
+    void OpenGraphics::setOrthoOn(OpenWindow window)
+    {
+        int width, height;
+        glfwGetFramebufferSize(window.getWindowPointer(), &width, &height);
+        glMatrixMode(GL_PROJECTION);
+        glPushMatrix(); // preserve perspective view
+        glLoadIdentity(); // clear the perspective matrix
+        glOrtho( // turn on 2D mode
+                //// viewportX,viewportX+viewportW, // left, right
+                //// viewportY,viewportY+viewportH, // bottom, top !!!
+                0, width, // left, right
+                height, 0, // bottom, top
+                -500, 500); // Zfar, Znear
+        // clear the modelview matrix
+        glMatrixMode(GL_MODELVIEW);
+        glPushMatrix(); // Preserve the Modelview Matrix
+        glLoadIdentity(); // clear the Modelview Matrix
+        // disable depth test so further drawing will go over the current scene
+        glDisable(GL_DEPTH_TEST);
+    }
+
+    void OpenGraphics::setOrthoOff()
+    {
+        // Restore the original positions and views.
+        glMatrixMode(GL_PROJECTION);
+        glPopMatrix();
+        glMatrixMode(GL_MODELVIEW);
+        glPopMatrix();
+        // Turn Depth Testing back on.
+        glEnable(GL_DEPTH_TEST);
+    }
+
+    void OpenGraphics::setTransparency(bool transparency)
+    {
+        if (transparency)
+        {
+            glEnable(GL_BLEND);
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        }
+        else
+            glDisable(GL_BLEND);
     }
 
     float toFloatColor(short255 color)
@@ -69,10 +122,10 @@ namespace Openw67Render
     void OpenGraphics::drawLine2D(float x, float y, float x2, float y2)
     {
         // Calculate the real X, the real Y, the real second X and the real second Y.
-        x += complementX;
-        y += complementY;
-        x2 += complementX;
-        y2 += complementY;
+        x += _complementX;
+        y += _complementY;
+        x2 += _complementX;
+        y2 += _complementY;
 
         // Start to draw the line.
         beginDraw(DRAW_TYPE_LINES);
