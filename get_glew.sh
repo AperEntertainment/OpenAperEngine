5bcaef558ed17c343d85e4390fdbcdd068882a41
adri326 <adrien.burgun@orange.fr>
2016-12-22 12:51:56 +0100

    Update get_glew.sh

--- get_glew.sh
+++ get_glew.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 # Go to dependencies
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo -e "${RED}Warning!${NC} Ubuntu users may need to install some additional packages, check the file fix_ubuntu.txt"
+
+
 cd dependencies/
 # Download glew 2
 wget http://downloads.sourceforge.net/project/glew/glew/2.0.0/glew-2.0.0.tgz
